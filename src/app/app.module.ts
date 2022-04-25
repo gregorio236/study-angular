72949c6bc8996783c3fe9ceedd4e454fba111769
Gregorio Ramos <gregorio236@gmail.com>
2022-04-24 22:18:25 -0300

    Create dropdown directive;

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,12 +8,14 @@ import { RecipeDetailComponent } from "./recipes/recipe-detail/recipe-detail.com
 import { RecipeItemComponent } from "./recipes/recipe-list/recipe-item/recipe-item.component";
 import { RecipeListComponent } from "./recipes/recipe-list/recipe-list.component";
 import { RecipesComponent } from "./recipes/recipes.component";
+import { DropdownDirective } from "./shared/dropdown.directive";
 import { ShoppingEditComponent } from "./shopping-list/shopping-edit/shopping-edit.component";
 import { ShoppingListComponent } from "./shopping-list/shopping-list.component";
 
 @NgModule({
   declarations: [
     AppComponent,
+    DropdownDirective,
     HeaderComponent,
     RecipeDetailComponent,
     RecipeItemComponent,
