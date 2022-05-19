c3eb511fbe57299c5b79c0dd3febabaab80687d5
Gregorio Ramos <gregorio236@gmail.com>
2022-05-19 17:52:07 -0300

    Implement lazy loading;

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,31 @@
 import { NgModule } from "@angular/core";
-import { RouterModule, Routes } from "@angular/router";
+import { PreloadAllModules, RouterModule, Routes } from "@angular/router";
 
 const appRoutes: Routes = [
   { path: "", redirectTo: "/recipes", pathMatch: "full" },
+  {
+    path: "recipes",
+    loadChildren: async () =>
+      await import("./recipes/recipes.module").then((m) => m.RecipesModule),
+  },
+  {
+    path: "shopping-list",
+    loadChildren: async () =>
+      await import("./shopping-list/shopping-list.module").then(
+        (m) => m.ShoppingListModule
+      ),
+  },
+  {
+    path: "auth",
+    loadChildren: async () =>
+      await import("./auth/auth.module").then((m) => m.AuthModule),
+  },
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(appRoutes)],
+  imports: [
+    RouterModule.forRoot(appRoutes, { preloadingStrategy: PreloadAllModules }),
+  ],
   exports: [RouterModule],
 })
 export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,25 +4,18 @@ import { BrowserModule } from "@angular/platform-browser";
 
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
-import { AuthModule } from "./auth/auth.module";
 import { CoreModule } from "./core.module";
 import { HeaderComponent } from "./header/header.component";
-import { RecipesModule } from "./recipes/recipes.module";
 import { SharedModule } from "./shared/shared.module";
-import { ShoppingListModule } from "./shopping-list/shopping-list.module";
 
 @NgModule({
   declarations: [AppComponent, HeaderComponent],
   imports: [
     HttpClientModule,
     BrowserModule,
-
     AppRoutingModule,
-    AuthModule,
     CoreModule,
-    RecipesModule,
     SharedModule,
-    ShoppingListModule,
   ],
   bootstrap: [AppComponent],
 })
